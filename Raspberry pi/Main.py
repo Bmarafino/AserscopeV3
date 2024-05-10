6731f91d83c93b6bb730e0bf0a9441e3d162d916
Zachary Moorman <zmoorman@udel.edu>
2024-05-09 20:36:25 -0400

    Allow for text input via IDE, will need to allow input in other way in the future

--- Raspberry pi/Main.py
+++ Raspberry pi/Main.py
@@ -4,8 +4,20 @@ from ImageEdgeClass import ImageProcessor
 from svg_player import SVGPlotter
 from buffervisulaiser import BufferVisualizer
 from svg2ild import SvgProcessor
+from letterTranslation import *
 
+def fix_text(input_text):
+    input_text = input_text.upper()
+    length_text = len(input_text)
+    points = []
+    for i in range(length_text):
+        modifier = i * 4096
+        for x in range(len(letter_dict[input_text[i]])):
+            if x * 3 % length_text == 0:
+                points.append(tuple([(letter_dict[input_text[i]][x][0] + modifier) // (length_text + 1), letter_dict[input_text[i]][x][1], i % 3 + 1 if letter_dict[input_text[i]][x][2] != 0 else 0]))
+    return points
 
+letter_dict = get_letter_dict()
 mainBuffer = Buffer()
 # print(ImageProcessor("/Users/bmarafino/Downloads/IMG_9945.jpg").get_points()[:100])
 # print(ImageProcessor("/Users/bmarafino/Downloads/udel2.png").get_points()[:100])
@@ -16,11 +28,17 @@ mainBuffer = Buffer()
 
 # mainBuffer.add("svg", png, plays=10)
 
+input_text = "Decision Day 2025"
+
+points = fix_text(input_text)
+
 # SVGPlotter("/Users/bmarafino/Documents/test.svg", 200).plot_svg()
 # SVGPlotter("/Users/bmarafino/Downloads/building-svgrepo-com.svg", 100).plot_svg()
-pointsObj = SvgProcessor("/Users/bmarafino/Documents/sendover/udel.svg").get_points()
+pointsObj = SvgProcessor("C:/Users/zachm/A.svg").get_points()
+print(pointsObj)
 # pointsObj = SVGPlotter("/Users/bmarafino/Downloads/apple.svg", 300).get_points()
-mainBuffer.add("svg", pointsObj, plays=10)
+#mainBuffer.add("svg", pointsObj, plays=10)
+mainBuffer.add("txt", points, plays=100)
 
 # # mainBuffer.add(
 #     "ild",
@@ -30,7 +48,7 @@ mainBuffer.add("svg", pointsObj, plays=10)
 
 
 # Assuming you have an instance of Buffer named `buffer_instance`
-buffer_visualizer = BufferVisualizer(mainBuffer, len(pointsObj))
+buffer_visualizer = BufferVisualizer(mainBuffer, len(letter_dict["C"]) + 1000) #This might need to be changed back but I was using it for testing letters
 buffer_visualizer.plot_static_frame()
 
 
